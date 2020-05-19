01a8fdd710a5bdb2d60869e40225ffc54825f79b
Ashish Mondal <ashishkrmondal@gmail.com>
2020-05-19 00:20:44 -0400

    Added spinner and signup service

--- auth-01-auth-component/src/app/auth/auth.component.ts
+++ auth-01-auth-component/src/app/auth/auth.component.ts
@@ -8,6 +8,8 @@ import { AuthService } from './auth.service';
 })
 export class AuthComponent {
   isLoginMode = true;
+  isLoading = false;
+  error: string = null;
 
   constructor(private authService: AuthService) {}
   onSwitchModel() {
@@ -24,15 +26,19 @@ export class AuthComponent {
     const email = form.value.email;
     const password = form.value.password;
 
+    this.isLoading = true;
     if(this.isLoginMode) {
 
     } else {
       this.authService.signup(email, password).subscribe(
         responseData => {
+          this.isLoading = false;
           console.log(responseData);
         },
         error => {
+          this.isLoading = false;
           console.log(error);
+          this.error = 'An error occured!';
         }
       );
     }
